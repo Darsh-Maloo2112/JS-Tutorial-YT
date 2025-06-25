215536fcced7e45dee7a978b4538df174d113e78
Darsh-Maloo2112 <darshmaloo2@gmail.com>
2025-06-25 15:08:46 +0000

    Numbers and Maths in JS

--- num-and-math.js
+++ num-and-math.js
@@ -1,8 +1,37 @@
 const score = 300
-console.log(score);
+// console.log(score);
 
-const newScore = new Number(500)
-console.log(newScore);
+const newScore = new Number(500.54856)
+// console.log(newScore);
 
-console.log(score.toString());
-console.log(score.toString().length);
+// console.log(score.toString());
+// console.log(score.toString().length);
+
+// console.log(score.toFixed(2));
+
+// console.log(newScore.toPrecision(4));
+
+const bigNumber = 100000000
+// console.log(bigNumber.toLocaleString('en-IN'));
+
+//----------------Maths---------------------
+
+// console.log(Math);
+
+// console.log(Math.abs(-2));
+
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.7));
+
+// console.log(Math.min(5,4,7,2,8,3));
+// console.log(Math.max(5,4,7,2,8,3));
+
+console.log((Math.random()*10) + 1)
+
+console.log(Math.floor(Math.random()));
+
+const min = 10
+const max = 20
+
+console.log( Math.floor(Math.random()* (max-min+1))+min);
