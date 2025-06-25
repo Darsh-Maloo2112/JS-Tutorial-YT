d667fd579870728a286396647a34d4251ce521c8
Darsh-Maloo2112 <darshmaloo2@gmail.com>
2025-06-25 15:38:19 +0000

    Dates in JS

---
+++ dates.js
@@ -0,0 +1,10 @@
+let date = new Date()
+// console.log(date.toString());
+// console.log(date.toDateString());
+
+let newDate = new Date(2025,7,3)
+// console.log(newDate.toDateString());
+
+let timeStamp = Date.now()
+
+console.log(timeStamp);
